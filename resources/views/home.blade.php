e59b4f2e562d1566963d6dc7c26e4eae64988606
David <dev.davidperez@gmail.com>
2021-12-13 19:33:01 +0100

    modification alignement de la navbar et du logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 
     <header class="hero">
         <img src="/images/photoheader.jpg" class="headerpic" />
+        <div class="tagline">Cuisine végétale et éco-responsable</div>
     </header>
     </body>
 </html>
