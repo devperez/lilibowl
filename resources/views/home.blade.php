845d8d954558061e1641a0b38b679789275458fd
David <dev.davidperez@gmail.com>
2021-12-21 12:04:47 +0100

    ajout d'un gradient sur le footer, ajout d'une déco sur engagements, modifs en cours sur le carrousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -95,15 +95,24 @@
                 <div class="engagementtitre">
                     <h2>Nos engagements</h2>
                 </div>
-                <ul>
-                    <li><p class="texteli">Proposer une cuisine sans gluten et végétale variée aux influences du monde.</p></li>
-                    <li><p class="texteli">Utiliser des produits majoritairement issus de l’agriculture biologique, sans pesticides et de saison.</p></li>
-                    <li><p class="texteli">Connaître les producteurs et privilégier les circuits courts pour éviter le gaspillage alimentaire et favoriser l’économie locale.</p></li>
-                    <li><p class="texteli">Respecter l’environnement dans une démarche éco-responsable (lunch boxes consignées en verre, tri sélectif, compost,
-                        revalorisation des déchets…).</p></li>
-                    <li><p class="texteli">Éduquer le plus grand nombre à l’alimentation santé et au goût.</p></li>
-                    <li><p class="texteli">Favoriser le lien social autours de projets, d’ateliers et de repas partagés.</p></li>
-                </ul>
+                <div class="wrapper_engagement">
+                    <div>
+                        <ul>
+                            <li><p class="texteli">Proposer une cuisine sans gluten et végétale variée aux influences du monde.</p></li>
+                            <li><p class="texteli">Utiliser des produits majoritairement issus de l’agriculture biologique, sans pesticides et de saison.</p></li>
+                            <li><p class="texteli">Connaître les producteurs et privilégier les circuits courts pour éviter le gaspillage alimentaire et favoriser l’économie locale.</p></li>
+                            <li><p class="texteli">Respecter l’environnement dans une démarche éco-responsable (lunch boxes consignées en verre, tri sélectif, compost,
+                                    revalorisation des déchets…).</p></li>
+                            <li><p class="texteli">Éduquer le plus grand nombre à l’alimentation santé et au goût.</p></li>
+                            <li><p class="texteli">Favoriser le lien social autours de projets, d’ateliers et de repas partagés.</p></li>
+                        </ul>
+                    </div>
+                    <div class="fleur_engagement">
+                        <img src="/images/fleur_homepage.png" alt="illustration florale" />
+                    </div>
+                </div>
+                    
+                </div>
             </section>
             <section class="container-frise">
                 <div class="marque">
@@ -207,51 +216,54 @@
                     <img src="/images/next.png" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">   
                 </div>
             </section>
-            <section class="footer">
-                <div class="newsletter">
-                    <p class="titre">RECEVOIR LA NEWSLETTER</p>
-                    <p class="texte">Recevez le nouveau menu de saison au début de chaque mois, ainsi que des informations
-                        sur nos ateliers et événements dans votre boîte mail :)
-                    </p>
-                </div>
-                <div class="inscription">
-                    <label>Email</label>
-                    <input type="email" placeholder="lili@mail.com">
-                    <button class="button"> Je m'inscris !</button>
-                </div>
-            </section>
+            <div class="last_section container-fluid">
+                <section class="footer">
+                    <div class="newsletter">
+                        <p class="titre">RECEVOIR LA NEWSLETTER</p>
+                        <p class="texte">Recevez le nouveau menu de saison au début de chaque mois, ainsi que des informations
+                            sur nos ateliers et événements dans votre boîte mail :)
+                        </p>
+                    </div>
+                    <div class="inscription">
+                        <label>Email</label>
+                        <input type="email" placeholder="lili@mail.com">
+                        <button class="button"> Je m'inscris !</button>
+                    </div>
+                </section>
+                <hr>
+                <footer id="contact">
+                    <div class="logo-contact-photo">
+                        <div class="logo-contact-icones">
+                            <div class="logolili">
+                                <a href="#logo"><img class="logo-footer" src="/images/Logo-Lilibowl.png" alt="logo de Lili BOwL" /></a>
+                            </div>
+                            <p class="contact">Contactez-nous</p>
+                            <address>12, rue Daurade, 46000 Cahors<br />
+                                    06.30.03.73.89<br />
+                                    lilibowl.cahors@gmail.com
+                            </address>
+                            <img class="insta" src="/images/Insta.png" alt="icône instagram" />
+                            <img src="/images/Fb.png" alt="icône facebook" />
+                            <div class="wrapper-horaires">
+                                <p class="horaires">Ouvert du lundi au samedi, de 11h à 15h, sur place ou à emporter.</p>
+                            </div>
+                        </div>
+                        <div class="photo-texte">
+                            <img src="/images/footer_photo.png" alt="photo de la terrasse du restaurant" />
+                        </div>
+                    </div>
+                    <div class="partagez-wrapper">
+                        <span class="partagez">Partagez votre expérience :</span>
+                        <img class="googleicon" src="/images/icone_google.png" alt="icône Google pour laisser un avis" />
+                        <img src="/images/icone_tripadvisor.png" alt="icône pour aller sur le site de Tripadvisor" />
+                    </div>
+                    <div class="mentions">
+                        <span>Lili BOwL &copy; Mentions légales - créé avec <span class="heart">&hearts;</span> par David PEREZ</span>
+                    </div>
+                </footer>
+            </div>
         </main>
-        <hr>
-        <footer id="contact">
-            <div class="logo-contact-photo">
-                <div class="logo-contact-icones">
-                    <div class="logolili">
-                        <a href="#logo"><img class="logo-footer" src="/images/Logo-Lilibowl.png" alt="logo de Lili BOwL" /></a>
-                    </div>
-                    <p class="contact">Contactez-nous</p>
-                    <address>12, rue Daurade, 46000 Cahors<br />
-                            06.30.03.73.89<br />
-                            lilibowl.cahors@gmail.com
-                    </address>
-                    <img class="insta" src="/images/Insta.png" alt="icône instagram" />
-                    <img src="/images/Fb.png" alt="icône facebook" />
-                    <div class="wrapper-horaires">
-                        <p class="horaires">Ouvert du lundi au samedi, de 11h à 15h, sur place ou à emporter.</p>
-                    </div>
-                </div>
-                <div class="photo-texte">
-                    <img src="/images/footer_photo.png" alt="photo de la terrasse du restaurant" />
-                </div>
-            </div>
-            <div class="partagez-wrapper">
-                <span class="partagez">Partagez votre expérience :</span>
-                <img class="googleicon" src="/images/icone_google.png" alt="icône Google pour laisser un avis" />
-                <img src="/images/icone_tripadvisor.png" alt="icône pour aller sur le site de Tripadvisor" />
-            </div>
-            <div class="mentions">
-                <span>Lili BOwL &copy; Mentions légales - créé avec <span class="heart">&hearts;</span> par David PEREZ</span>
-            </div>
-        </footer>
+        </div>
 
         <!-- POP UP -->
 
