62f775ca9315b27de5de59faac10f5e6b693b114
devperez <advitameternam@gmail.com>
2022-01-18 18:55:35 +0100

    ajout d'un script pour déconnecter automatiquement l'utilisateur au bout de 15 minutes d'inactivité

--- resources/views/displaymails.blade.php
+++ resources/views/displaymails.blade.php
@@ -71,5 +71,22 @@ $(document).ready(function (){
         })
     })
     
+
+
+// autologout.js
+
+$(document).ready(function () {
+    const timeout = 900000;  // 900000 ms = 15 minutes
+    var idleTimer = null;
+    $('*').bind('mousemove click mouseup mousedown keydown keypress keyup submit change mouseenter scroll resize dblclick', function () {
+        clearTimeout(idleTimer);
+
+        idleTimer = setTimeout(function () {
+            document.getElementById('form_id').submit();
+        }, timeout);
+    });
+    $("body").trigger("mousemove");
+});
+
 </script>
 @endsection
