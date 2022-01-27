96d5f4a0fd7d81f7b06e17d9c8b8fbaf9ff9fece
devperez <advitameternam@gmail.com>
2022-01-27 19:05:27 +0100

    début mise en place de la collecte de mails par le footer

--- public/js/newsletter.js
+++ public/js/newsletter.js
@@ -1,49 +1,53 @@
+//Affichage de la modale au bout de 5 secondes
+
 $(document).ready(function(){
     setTimeout(function(){
     $('#myModal').modal('show');
     },5000);
 })
 
-//script pour récupérer l'adresse mail
+//script pour récupérer l'adresse mail dans la pop-up
 
 $(document).ready(function () {
-    $("form").submit(function(event) {  
+    $("#form").submit(function(event) {  
         event.preventDefault();
 
-    $('#error').addClass().css('display','none');
+        $('#error').addClass().css('display','none');
 
-    var formData = {
-        email: $("#email").val(),
-        _token: $('input[name="_token"]').val(),
-    };
+        var formData = {
+            email: $("#email").val(),
+            _token: $('input[name="_token"]').val(),
+        };
 
-console.log(formData);
-$.ajax({
-    type:"POST",
-    url: "{{ route(`getmail`) }}",
-    data: formData,
-    dataType: "json",
-    encode:true,
-})
-.done(function() {
-    console.log('ok');
-    $('#recevez').addClass().css('display','none');
-    $('#merci').addClass().css('display','block');
-    $('#confirm').addClass().css('display','block');
-    $('#inscription').addClass().css('display', 'none');
-    $('#form').addClass().css('display', 'none');
-    $('.ellipse1').addClass().css('display', 'block');
-    $('.ellipse2').addClass().css('display', 'block');
+// console.log(formData);
+    $.ajax({
+        type:"POST",
+        url: "http://127.0.0.1:8000/getmail",
+        data: formData,
+        dataType: "json",
+        encode:true,
+    })
+    .done(function() {
+        console.log('done');
+        $('#recevez').addClass().css('display','none');
+        $('#merci').addClass().css('display','block');
+        $('#confirm').addClass().css('display','block');
+        $('#inscription').addClass().css('display', 'none');
+        $('#form').addClass().css('display', 'none');
+        $('.ellipse1').addClass().css('display', 'block');
+        $('.ellipse2').addClass().css('display', 'block');
 
-    setTimeout(function(){
-        $('#myModal').modal('hide');
-    },5000);
-})
-.fail(function() {
-    $('#error').addClass().css('display','flex');
-    $('.label').addClass().css('width', '50%');
-    $('.form').addClass().css('align-items', 'center');
-    console.log('fail');
+        setTimeout(function(){
+            $('#myModal').modal('hide');
+        },5000);
+    })
+    .fail(function() {
+        $('#error').addClass().css('display','flex');
+        $('.label').addClass().css('width', '50%');
+        $('.form').addClass().css('align-items', 'center');
+        console.log('undone');
+        });
     });
 });
-});        
+
+
