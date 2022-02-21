4d42b3c3a851c7087c8641d019f6cdb3109802f8
devperez <advitameternam@gmail.com>
2022-02-21 18:54:47 +0100

    responsive design pour la homepage, pour le restaurant ok, menu mobile pour les pages sensibilisation et formations ok et début responsive pour la page menu

--- resources/views/sensibilisation.blade.php
+++ resources/views/sensibilisation.blade.php
@@ -19,7 +19,8 @@
         <link rel="stylesheet" href="/css/navbar.css" />
         <link rel="stylesheet" href="/css/footer.css" />
         <link rel="stylesheet" href="/css/restaurant_styles.css" />
-        
+        <link rel="stylesheet" href="/css/navbar_mobile.css" />
+
         <!--JQuery-->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         
