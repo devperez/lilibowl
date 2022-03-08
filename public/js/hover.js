86fa3e4542df82b403aec8d24b13a66461059a7b
devperez <advitameternam@gmail.com>
2022-03-08 12:10:54 +0100

    poursuite de la page formations

---
+++ public/js/hover.js
@@ -0,0 +1,11 @@
+$(".lien_image1").hover(function(){
+    $(".lien_image1").attr('src','/images/formations/formations_lien_atelier_hover.png');
+},function(){
+    $(".lien_image1").attr("src", "/images/formations/formations_lien_atelier.png");
+});
+
+$(".lien_image2").hover(function(){
+    $(".lien_image2").attr('src','/images/formations/formations_lien_initiationpro_hover.png');
+},function(){
+    $(".lien_image2").attr("src", "/images/formations/formations_lien_initiationpro.png");
+});
