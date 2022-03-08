2bdb6c8aea52549f8d568ccbc4d81f59cff3d228
devperez <advitameternam@gmail.com>
2022-03-08 17:33:57 +0100

    ajout de meta tags pour fb et twitter en cours, ajout de liens, modif de l'apparence des liens dans le footer

--- resources/views/sensibilisation.blade.php
+++ resources/views/sensibilisation.blade.php
@@ -4,7 +4,26 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Lili BOwL | Projets de sensibilisation</title>
+        <meta name="description" content= "Lili BOwL | Restaurant végétarien engagé dans la promotion d'une cuisine bio, saine et éco-responsable." />
+        <meta name="robots" content= "index, follow">
+        
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://???????">
+        <meta property="og:title" content="Restaurant et traiteur végétarien à Cahors | Lili BOwL">
+        <meta property="og:description" content="Lili BOwL | Restaurant/traiteur végétarien éco-responsable proposant une cuisine végétale vitaminée et colorée">
+        <meta property="og:image" content="/images/image_social_media_tags.jpg">
+
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content=" https://???????">
+        <meta property="twitter:title" content="Restaurant et traiteur végétarien à Cahors | Lili BOwL">
+        <meta property="twitter:description" content="Lili BOwL | Restaurant/traiteur végétarien éco-responsable proposant une cuisine végétale vitaminée et colorée">
+        <meta property="twitter:image" content="adresse image">
+        
+        <title>Restaurant et traiteur végétarien à Cahors | Lili BOwL</title>
+        
         <!--FONT-->
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Karla:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
         
@@ -18,8 +37,8 @@
         <!-- CSS -->   
         <link rel="stylesheet" href="/css/navbar.css" />
         <link rel="stylesheet" href="/css/footer.css" />
-        <link rel="stylesheet" href="/css/navbar_mobile.css" />
         <link rel="stylesheet" href="/css/sensibilisation_styles.css">
+        <link rel="stylesheet" href="/css/navbar_mobile.css" />
 
         <!--JQuery-->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
